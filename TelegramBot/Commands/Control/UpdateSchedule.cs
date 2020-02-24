6d347c3e8bb0c3e592472e20f13258470e155553
NiJeTi <nijeti.inc@gmail.com>
2020-02-24 15:33:57 +0300

    Command that uploads new schedule file and maps parser to it

---
+++ TelegramBot/Commands/Control/UpdateSchedule.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+using ScheduleParser;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+using TelegramBot.Resources.Localizations;
+
+using File = System.IO.File;
+
+namespace TelegramBot.Commands.Control
+{
+    internal sealed class UpdateSchedule : ICommand
+    {
+        private const string FileSavePath = "Resources/";
+
+        private readonly ParserCore         parserCore;
+        private readonly ITelegramBotClient bot;
+        private readonly CancellationToken  cancellationToken;
+
+        public string Response { get; private set; }
+
+        public UpdateSchedule(ParserCore parserCore, ITelegramBotClient bot, CancellationToken cancellationToken)
+        {
+            this.parserCore        = parserCore;
+            this.bot               = bot;
+            this.cancellationToken = cancellationToken;
+
+            Response = string.Empty;
+        }
+
+        public void Prepare()
+        {
+            Response = LocalizationManager.GetLocalizedText("FileRequest");
+        }
+
+        public void Handle(object query)
+        {
+            if (!(query is Message))
+                throw new NotSupportedException("Only message queries supported");
+
+            var message = (Message) query;
+
+            if (message.Document != null)
+            {
+                if (Path.GetExtension(message.Document.FileName).ToLower() != ".xlsx")
+                {
+                    Response = LocalizationManager.GetLocalizedText("FileWrongType");
+
+                    return;
+                }
+
+                var    recievedFile = bot.GetFileAsync(message.Document.FileId, cancellationToken).GetAwaiter().GetResult();
+                string filePath     = FileSavePath + message.Document.FileName;
+
+                using (var fileSaveStream = File.Open(filePath, FileMode.Create))
+                    bot.DownloadFileAsync(recievedFile.FilePath, fileSaveStream, cancellationToken).GetAwaiter().GetResult();
+
+                parserCore.SetUp(filePath);
+
+                Response = LocalizationManager.GetLocalizedText("ScheduleUpdated");
+            }
+            else
+            {
+                Response = LocalizationManager.GetLocalizedText("NoDocument");
+            }
+        }
+    }
+}

a4b4d9c5e3b212ad5e909ea5e86b81e7c8644b5e
NiJeTi <nijeti.inc@gmail.com>
2020-02-24 15:26:50 +0300

    Services extension class containing parser dependency injection

---
+++ TelegramBot/ServicesExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using ScheduleParser;
+
+using Serilog;
+
+namespace TelegramBot
+{
+    internal static class ServicesExtensions
+    {
+        public static IServiceCollection AddScheduleParser(this IServiceCollection services, string scheduleFile)
+        {
+            var parserCore = new ParserCore();
+
+            parserCore.SetUp(scheduleFile);
+
+            Log.Information("Parser setup successful");
+
+            return services.AddSingleton(parserCore);
+        }
+    }
+}

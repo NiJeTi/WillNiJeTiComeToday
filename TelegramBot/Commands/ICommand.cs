b9e43392ed48a50f1b33022badf5e8ed9a3713ef
NiJeTi <nijeti.inc@gmail.com>
2020-02-24 15:32:35 +0300

    Bot command interface

---
+++ TelegramBot/Commands/ICommand.cs
@@ -0,0 +1,11 @@
+﻿namespace TelegramBot.Commands
+{
+    internal interface ICommand
+    {
+        public string Response { get; }
+
+        public void Prepare();
+
+        public void Handle(object query);
+    }
+}
